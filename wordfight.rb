df9c01557cf8293f8cff626cb2127d7a27a6c404
Gareth Fleming <d3z@joocy.com>
2012-04-24 10:31:57 -0400

    used the cache to keep websocket connections handy

--- wordfight.rb
+++ wordfight.rb
@@ -1,26 +1,27 @@
 require 'rack/websocket'
+require './cache.rb'
 
 class WordFight < Rack::WebSocket::Application
-    @user=''
     def on_open(env)
         @user = env['PATH_INFO'].gsub('/','')
-        close_websocket if @user.empty? or user_connected?
+        close_websocket if @user.empty? or Cache::connection_cached_for?(@user)
         puts "#{@user} is connected"
+        Cache::cache_connection_for(@user, self)
     end
 
     def on_close(env)
         puts "Closed connection to #{@user}"
+        Cache::remove_connection_for(@user)
     end
 
     def on_message(env, msg)
-        puts "#{@user} said '#{msg}'"
+        to_user, data = msg.split(':')
+        puts "#{@user} is sending '#{data}' to #{to_user}"
+        Cache::get_connection_for(to_user).send_data(data)
     end
 
     def on_error(env, error)
     end
 
-    def user_connected?
-        return false
-    end
 end
 
