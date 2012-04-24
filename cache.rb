3a01894b9ea32021939995b2acf52f2222f707b1
Gareth Fleming <d3z@joocy.com>
2012-04-24 10:27:56 -0400

    added the cache to make websocket connections available to the application

---
+++ cache.rb
@@ -0,0 +1,19 @@
+class Cache
+    @@cache = {}
+
+    def self.cache_connection_for(user, connection)
+        @@cache[user] = connection
+    end
+
+    def self.remove_connection_for(user)
+        @@cache.delete(user)
+    end
+
+    def self.get_connection_for(user)
+        return @@cache[user]
+    end
+
+    def self.connection_cached_for?(user)
+        return @@cache.has_key?(user)
+    end
+end
